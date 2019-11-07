063ac5360cddd9ad54791d2d768e506db8e62a64
SarvM <saravana.msi@outlook.com>
2019-11-07 20:20:11 +0530

    generic service call

--- src/app/config-file/config-file.component.ts
+++ src/app/config-file/config-file.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Settings, ConfigFileService } from './config-file.service';
 
 @Component({
   selector: 'app-config-file',
   templateUrl: './config-file.component.html',
-  styleUrls: ['./config-file.component.css']
+  styleUrls: ['./config-file.component.css'],
+  providers: [ConfigFileService]
 })
 export class ConfigFileComponent implements OnInit {
 
-  constructor() { }
+  settings: Settings;
+
+  constructor(private settingsService: ConfigFileService) { }
 
   ngOnInit() {
+  }
+
+  clear() {
+    this.settings = undefined;
+  }
+
+  get diagnostic() { return JSON.stringify(this.settings); }
+
+  showSettings() {
+    this.settingsService.getSettingsValue()
+      .subscribe(
+        (data: Settings) => this.settings = { ...data },         
+      );
   }
 
 }
--- src/app/config-file/config-file.service.ts
+++ src/app/config-file/config-file.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 export interface Settings {
   apiUrl: string,
@@ -9,5 +10,14 @@ export interface Settings {
 
 @Injectable()
 export class ConfigFileService {
+  url = 'assets/settings.json';
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  getSettingsValue() {
+    return this.http.get(this.url);
+  }
 
 }
